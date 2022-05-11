94e471be3eccfab12ea116b4571e3bffdddf8a0c
Anna Wróbel <ania.ulanecka@gmail.com>
2022-05-11 18:56:59 +0200

    update mail scripts

--- birthday.js
+++ birthday.js
@@ -1,14 +1,9 @@
-var mysql = require("mysql");
-var nodemailer = require('nodemailer');
-var express = require('express');
-const utf8 = require('utf8');
-var app = express();
-const {StringDecoder} = require("string_decoder");
-const decoder = new StringDecoder('utf8');
-const uffer = require("buffer");
-//var mail = require('./mail.js');
-var port = 3306;
-var con = mysql.createConnection({
+let mysql = require("mysql");
+let nodemailer = require('nodemailer');
+let express = require('express');
+let app = express();
+let sender = 'ania.ulanecka@gmail.com';
+let con = mysql.createConnection({
   host: "az-serwer1749470.online.pro",
   user: "00062688_ania",
   password: "f8IyaYM1",
@@ -31,7 +26,7 @@ con.connect((error) =>
 
 con.connect(function (error){
   con.query("SET NAMES utf8mb4");
-  con.query("SELECT family_id, family_id_2, CONCAT(name,'-', lastname) AS name, YEAR(CURDATE())-substring(date_of_birth, 7) as age, CONCAT(day,'-',month) AS data FROM 00062688_ania.birthday WHERE (CONCAT(day,'-',month)) = DATE_FORMAT(NOW(), '%d-%m')",
+  con.query("SELECT mail, family_id, family_id_2, CONCAT(name,'-', lastname) AS name, YEAR(CURDATE())-substring(date_of_birth, 7) AS age FROM 00062688_ania.birthday WHERE LEFT(date_of_birth, 5) = DATE_FORMAT(NOW(), '%d-%m')",
     function dane(error, rows, fields)
     {
       if(!!error)
@@ -45,103 +40,68 @@ con.connect(function (error){
       else if (rows.length >= 1)
       {
         console.log('SUCCESS');
-        var name = rows[0].name;
-        var data = rows[0].data;
-        var family_id = rows[0].family_id;
-        var family_id_2 = rows[0].family_id_2;
-        var age = rows[0].age;
-        var title = 'Alert urodzinowy !!!';
-        var mail_message = 'Dziś są urodziny: ' + name + "\r\n" + 'Kończy dzisiaj -> ' + "\r\n" + age + ' lata' + "\r\n" + 'Nie zapomnij złożyć życzeń!!!';
-        var mail_message_html = '<img src="cid:birth@day.jpg" alt="birthday"/><p style="font-size:120%;font-family:Arial Black">Dziś są urodziny: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + name + '</b></p>' + '<p style="font-size:120%;font-family:Arial Black">Kończy dzisiaj: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + age + ' lata</b></p>' + '<p style="font-size:140%;font-family:Arial Black"><b>Nie zapomnij złożyć życzeń!!!</b></p>';
-        // wysłanie maila
-        var transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: '587',
-          service: 'Gmail',
-          auth: {
-            user: 'ania.ulanecka@gmail.com',
-            pass: 'Pingwin14@'
+        for(let row in rows) {
+          let name = rows[row].name;
+          let mail = rows[row].mail;
+          let family_id = rows[row].family_id;
+          let family_id_2 = rows[row].family_id_2;
+          let age = rows[row].age;
+          let title = 'Alert urodzinowy !!!';
+          let mail_message = 'Dziś są urodziny: ' + name + "\r\n" + 'Kończy dzisiaj -> ' + "\r\n" + age + ' lata' + "\r\n" + 'Nie zapomnij złożyć życzeń!!!';
+          let mail_message_html = '<img src="cid:birth@day.jpg" alt="birthday"/><p style="font-size:120%;font-family:Arial Black">Dziś są urodziny: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + name + '</b></p>' + '<p style="font-size:120%;font-family:Arial Black">Kończy dzisiaj: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + age + ' lata</b></p>' + '<p style="font-size:140%;font-family:Arial Black"><b>Nie zapomnij złożyć życzeń!!!</b></p>';
+          
+          // wysłanie maila
+          let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: '587',
+            service: 'Gmail',
+            auth: {
+              user: sender,
+              pass: 'Pingwin14@'
+            }
+          });
+
+          if (family_id_2 !== null) {
+            query = "SELECT mail FROM 00062688_ania.birthday WHERE mail IS NOT NULL AND (family_id IN (" + family_id + "," + family_id_2 + ") OR family_id_2 IN ( " + family_id + "," + family_id_2 + "))";
           }
-        });
-      if(family_id == 1 || family_id_2 == 1)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: 'ania.ulanecka@gmail.com',
-            //['ania.ulanecka@gmail.com', 'mulanecka@gmail.com', 'ola.ulanecka@gmail.com', 'noflyno@gmail.com', 'michal.p.kaczynski@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'birthday.jpg',
-              path: __dirname + '/birthday.jpg',
-              cid: 'birth@day.jpg'
-            }]
-          };
+          else {
+            query = "SELECT mail FROM 00062688_ania.birthday WHERE mail IS NOT NULL AND (family_id = " + family_id + " OR family_id_2 = " + family_id + ")";
+          }
+          con.query(query,
+          function dane(error, rows, fields)
+            {
+              let mails = [];
+              if(error) console.log("error loading emails");
+              else if(rows.length >= 1) {
+                for (let row in rows) {
+                  if(rows[row].mail !== mail) mails.push(rows[row].mail);
+                }
+              }
+               let mailOption = {
+                 from: sender,
+                 to: mails,
+                 subject: title,
+                 text: mail_message,
+                 html: mail_message_html,
+                 attachments: [{
+                   filename: 'birthday.jpg',
+                   path: __dirname + '/birthday.jpg',
+                   cid: 'birth@day.jpg'
+                 }]
+               };
 
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
+              transporter.sendMail(mailOption, (error,info)=>
+              {
+                if(!!error){
+                  console.log('ERROR z wysŁaniem maila:' + error);
+                }
+                else {
+                  console.log('Mail został wysłany: ' + info);
+                }
+              });
             }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
+          );
         }
-        /*if(family_id == 2 || family_id_2 == 2)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: ['ania.ulanecka@gmail.com', 'noflyno@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'birthday.jpg',
-              path: __dirname + '/birthday.jpg',
-              cid: 'birth@day.jpg'
-            }]
-          };
-
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
-            }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
-        }
-      if(family_id == 3 || family_id_2 == 3)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: ['mulanecka@gmail.com', 'michal.p.kaczynski@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'birthday.jpg',
-              path: __dirname + '/birthday.jpg',
-              cid: 'birth@day.jpg'
-            }]
-          };
-
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
-            }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
-        }*/
       }
     });
 });
-
-
-//f8IyaYM1
--- nameday.js
+++ nameday.js
@@ -2,11 +2,6 @@ var mysql = require("mysql");
 var nodemailer = require('nodemailer');
 var express = require('express');
 var app = express();
-const {StringDecoder} = require("string_decoder");
-const decoder = new StringDecoder('utf8');
-const uffer = require("buffer");
-//var mail = require('./mail.js');
-var port = 3306;
 var con = mysql.createConnection({
   host: "az-serwer1749470.online.pro",
   user: "00062688_ania",
@@ -30,7 +25,7 @@ con.connect((error) =>
 
 con.connect(pobranie = function (error){
   con.query("SET NAMES utf8mb4");
-  con.query("SELECT family_id, family_id_2, CONCAT(name,' ',lastname) AS name, CONCAT(day,'-', month) AS data FROM 00062688_ania.nameday WHERE (CONCAT(day,'-',month)) = DATE_FORMAT(NOW(), '%d-%m')",
+  con.query("SELECT mail, family_id, family_id_2, CONCAT(name,' ',lastname) AS name, date FROM 00062688_ania.nameday WHERE date = DATE_FORMAT(NOW(), '%d-%m')",
     function dane(error, rows, fields)
     {
       if(!!error)
@@ -44,104 +39,68 @@ con.connect(pobranie = function (error){
       else if (rows.length >= 1)
       {
         console.log('SUCCESS');
-        var name = rows[0].name;
-        var data = rows[0].data;
-        var family_id = rows[0].family_id;
-        var family_id_2 = rows[0].family_id_2;
-        var age = rows[0].age;
-        var title = 'Alert imieninowy !!!';
-        var mail_message = 'Dziś są imieniny: '+name+ "\r\n"+ 'Nie zapomnij złożyć życzeń!!!';
-        var mail_message_html = '<img src="cid:name@day.jpg" style="height:20%;width:20%" alt="nameday"/><p style="font-size:120%;font-family:Arial Black">Dziś są imieniny: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + name + '</b></p>' + '<p style="font-size:140%;font-family:Arial Black"><b>Nie zapomnij złożyć życzeń!!!</b></p>';
+        for (let row in rows) {
+          var name = rows[row].name;
+          let mail = rows[row].mail;
+          var family_id = rows[row].family_id;
+          var family_id_2 = rows[row].family_id_2;
+          var title = 'Alert imieninowy !!!';
+          var mail_message = 'Dziś są imieniny: '+name+ "\r\n"+ 'Nie zapomnij złożyć życzeń!!!';
+          var mail_message_html = '<img src="cid:name@day.jpg" style="height:20%;width:20%" alt="nameday"/><p style="font-size:120%;font-family:Arial Black">Dziś są imieniny: </p>' + '<p style="font-size:200%;color:#ad75ad;font-family:Arial Black"><b>' + name + '</b></p>' + '<p style="font-size:140%;font-family:Arial Black"><b>Nie zapomnij złożyć życzeń!!!</b></p>';
+  
+          // wysłanie maila
+          var transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: '587',
+            service: 'Gmail',
+            auth: {
+              user: 'ania.ulanecka@gmail.com',
+              pass: 'Pingwin14@'
+            }
+          });
 
-        // wysłanie maila
-        var transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: '587',
-          service: 'Gmail',
-          auth: {
-            user: 'ania.ulanecka@gmail.com',
-            pass: 'Pingwin14@'
+          if (family_id_2 !== null) {
+            query = "SELECT mail FROM 00062688_ania.nameday WHERE mail IS NOT NULL AND (family_id IN (" + family_id + "," + family_id_2 + ") OR family_id_2 IN ( " + family_id + "," + family_id_2 + "))";
           }
-        });
-      if(family_id == 1 || family_id_2 == 1)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: 'ania.ulanecka@gmail.com',
-            // ['ania.ulanecka@gmail.com', 'mulanecka@gmail.com', 'ola.ulanecka@gmail.com', 'noflyno@gmail.com', 'michal.p.kaczynski@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'nameday.jpg',
-              path: __dirname + '/nameday.jpg',
-              cid: 'name@day.jpg'
-            }]
-          };
+          else {
+            query = "SELECT mail FROM 00062688_ania.nameday WHERE mail IS NOT NULL AND (family_id = " + family_id + " OR family_id_2 = " + family_id + ")";
+          }
 
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
-            }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
+          con.query(query,
+            function dane(error, rows, fields)
+              {
+                let mails = [];
+                if(error) console.log("error loading emails");
+                else if(rows.length >= 1) {
+                  for (let row in rows) {
+                    if(rows[row].mail !== mail) mails.push(rows[row].mail);
+                  }
+                }
+   
+                let mailOption = {
+                  from: 'ania.ulanecka@gmail.com',
+                  to: ['ania.ulanecka@gmail.com', 'mulanecka@gmail.com', 'ola.ulanecka@gmail.com', 'noflyno@gmail.com', 'michal.p.kaczynski@gmail.com', 'sebastianno.cc@gmail.com'],
+                  subject: title,
+                  text: mail_message,
+                  html: mail_message_html,
+                  attachments: [{
+                    filename: 'nameday.jpg',
+                    path: __dirname + '/nameday.jpg',
+                    cid: 'name@day.jpg'
+                  }]
+                };
+      
+                transporter.sendMail(mailOption, (error,info)=>
+                {
+                  if(!!error){
+                    console.log('ERROR z wysŁaniem maila:' + error);
+                  }
+                  else {
+                    console.log('Mail został wysłany: ' + info);
+                  }
+                });
+              });
         }
-      /*if(family_id == 2 || family_id_2 == 2)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: ['ania.ulanecka@gmail.com', 'noflyno@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'nameday.jpg',
-              path: __dirname + '/nameday.jpg',
-              cid: 'name@day.jpg'
-            }]
-          };
-
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
-            }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
-        }
-      if(family_id == 3 || family_id_2 == 3)
-        {
-          var mailOption = {
-            from: 'ania.ulanecka@gmail.com',
-            to: ['mulanecka@gmail.com', 'michal.p.kaczynski@gmail.com'],
-            subject: title,
-            text: mail_message,
-            html: mail_message_html,
-            attachments: [{
-              filename: 'nameday.jpg',
-              path: __dirname + '/nameday.jpg',
-              cid: 'name@day.jpg'
-            }]
-          };
-
-          transporter.sendMail(mailOption, (error,info)=>
-          {
-            if(!!error){
-              console.log('ERROR z wysŁaniem maila:' + error);
-            }
-            else {
-              console.log('Mail został wysłany: ' + info);
-            }
-          });
-        }*/
       }
     });
 });
-
-
-//f8IyaYM1
